02d7b4293955e9b4f0979deef432cac7507a9038
ousemaakacha <akachaousema@gmail.com>
2025-10-07 13:56:01 +0200

    added comments

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -8,13 +8,20 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
 // scrivo numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
+  // multipli di 3 e 5 (MCM)
   if (i % 15 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 5 === 0) {
+  }
+  //   multipli di 5
+  else if (i % 5 === 0) {
     console.log("Buzz");
-  } else if (i % 3 === 0) {
+  }
+  //  multipli di 3
+  else if (i % 3 === 0) {
     console.log("Fizz");
-  } else {
+  }
+  // tutti i numeri
+  else {
     console.log(i);
   }
 }
