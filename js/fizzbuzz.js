0d07817b14c332c19391861e93a2975a05e52682
ousemaakacha <akachaousema@gmail.com>
2025-10-07 13:52:28 +0200

    multiples of 5

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -10,5 +10,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 for (let i = 1; i <= 100; i++) {
   if (i % 15 === 0) {
     console.log(FizzBuzz);
+  } else if (i % 5 === 0) {
+    console.log(Buzz);
   }
 }
