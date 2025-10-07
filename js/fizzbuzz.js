f7967873cdef387ec8e82d4c463127ffe0c2597a
ousemaakacha <akachaousema@gmail.com>
2025-10-07 13:54:27 +0200

    fixes

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -9,11 +9,11 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 // scrivo numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
   if (i % 15 === 0) {
-    console.log(FizzBuzz);
+    console.log("FizzBuzz");
   } else if (i % 5 === 0) {
-    console.log(Buzz);
+    console.log("Buzz");
   } else if (i % 3 === 0) {
-    console.log(Fizz);
+    console.log("Fizz");
   } else {
     console.log(i);
   }
